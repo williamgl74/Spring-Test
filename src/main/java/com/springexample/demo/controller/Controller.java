426e1681b0aacdae122ae21dcbe7689aee1362b8
william <william8719gl@gmail.com>
2022-02-08 17:34:46 -0500

    first commit

---
+++ src/main/java/com/springexample/demo/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.springexample.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    private String testInit() {
+        return "Inicial";
+    }
+
+    @GetMapping("/hello")
+    private String testHello() {
+        return "Hello";
+    }
+}
